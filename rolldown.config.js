93216defdf069e34a7bf932231661091a99ee29d
Kirk Eaton <contact@kirkeaton.ca>
2026-07-25 17:18:00 -0400

    feat: Migrate to ESM

---
+++ rolldown.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'rolldown';
+
+export default defineConfig({
+  input: 'src/main.ts',
+  output: {
+    cleanDir: true,
+    file: 'dist/index.js',
+    format: 'esm',
+    minify: false,
+  },
+  platform: 'node',
+});
